752f3152217614799a5aba1d0405bb5ce2506bf9
Yuri Drabik <yurist38@gmail.com>
2026-05-11 18:42:22 +0900

    Init v.0

---
+++ entrypoints/popup/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main className="placeholder-page">
+      <h1>Browser Extension Starter</h1>
+      <p>Replace this placeholder with your popup UI.</p>
+    </main>
+  );
+}
+
+export default App;
